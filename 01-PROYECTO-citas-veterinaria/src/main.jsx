54d41de6ca52d8591145abcd9aa4e7d961b71efa
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2024-01-18 16:40:08 -0300

    limpieza de archivos de proyecto vite

--- 01-PROYECTO-citas-veterinaria/src/main.jsx
+++ 01-PROYECTO-citas-veterinaria/src/main.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react' // API de react
+import ReactDOM from 'react-dom/client' // version web de react
 import App from './App.jsx'
 import './index.css'
 
