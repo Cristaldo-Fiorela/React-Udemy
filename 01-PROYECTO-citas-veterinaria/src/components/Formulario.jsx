20376b9d37af136dcf127466c8a11fef3f9f4da6
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2024-01-25 15:15:32 -0300

    Primeros pasos en sintaxis de eventos en React

--- 01-PROYECTO-citas-veterinaria/src/components/Formulario.jsx
+++ 01-PROYECTO-citas-veterinaria/src/components/Formulario.jsx
@@ -2,9 +2,13 @@ import { useState, useEffect } from 'react';
 
 function Formulario() {
 
-  const [nombre, setNombre] = useState('Donna');
+  const [nombre, setNombre] = useState('');
 
-  console.log(nombre);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('Enviando formulario');
+  }
 
   return (
     <div className="md:w-1/2 lg:w-2/5">
@@ -15,7 +19,10 @@ function Formulario() {
         <span className="text-indigo-600 font-bold">Administralos</span>
       </p>
 
-      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+      <form
+        className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+        onSubmit={ handleSubmit }
+      >
 
         <div className="mb-5">
           <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">Nombre Mascota</label>
@@ -24,6 +31,8 @@ function Formulario() {
             id="mascota" 
             placeholder="Nombre de la Mascota"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={ nombre }
+            onChange={ (e) => setNombre(e.target.value) }
           />
         </div>
 
