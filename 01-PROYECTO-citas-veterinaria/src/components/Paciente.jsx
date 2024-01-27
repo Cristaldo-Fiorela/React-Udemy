c619e488095bbb334377dafece50064fa343a910
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2024-01-26 21:07:07 -0300

    mostrando la informacio cargada en el listado de pacientes

--- 01-PROYECTO-citas-veterinaria/src/components/Paciente.jsx
+++ 01-PROYECTO-citas-veterinaria/src/components/Paciente.jsx
@@ -1,24 +1,25 @@
-const Paciente = () => {
+const Paciente = ({ paciente }) => {
+  const {nombre, propietario, email, fecha, sintomas} = paciente;
   return (
     <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
       <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {""}
-        <span className="font-normal normal-case">Donna</span>
+        <span className="font-normal normal-case">{ nombre }</span>
       </p>
 
       <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {""}
-        <span className="font-normal normal-case">Fio</span>
+        <span className="font-normal normal-case">{ propietario }</span>
       </p>
 
       <p className="font-bold mb-3 text-gray-700 uppercase">Email: {""}
-        <span className="font-normal normal-case">correo@correo.com</span>
+        <span className="font-normal normal-case">{ email }</span>
       </p>
 
       <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: {""}
-        <span className="font-normal normal-case">23 enero de 2024</span>
+        <span className="font-normal normal-case">{ fecha }</span>
       </p>
 
       <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas: {""}
-        <span className="font-normal normal-case">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae pariatur, alias ab veritatis excepturi at impedit veniam culpa magnam totam velit quaerat.</span>
+        <span className="font-normal normal-case">{ sintomas }</span>
       </p>
     </div>  
   )
