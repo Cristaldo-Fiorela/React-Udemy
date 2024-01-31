86e9470a3926d7f219473682998a42eda54ee71c
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2024-01-31 20:52:53 -0300

    eliminando paciente creado

--- 01-PROYECTO-citas-veterinaria/src/components/Paciente.jsx
+++ 01-PROYECTO-citas-veterinaria/src/components/Paciente.jsx
@@ -1,5 +1,7 @@
-const Paciente = ({ paciente, setPaciente }) => {
-  const {nombre, propietario, email, fecha, sintomas} = paciente;
+const Paciente = ({ paciente, setPaciente, eliminarPaciente }) => {
+
+  const {nombre, propietario, email, fecha, sintomas, id} = paciente;
+
   return (
     <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
       <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {""}
@@ -29,9 +31,10 @@ const Paciente = ({ paciente, setPaciente }) => {
           onClick={() => setPaciente(paciente)}
         >Editar</button>
 
-        <button 
+        <button
           type="button"
           className="py-2 px-10 bg-red-600 hover:bg-red-700 uppercase text-white font-bold rounded-lg"
+          onClick={() => eliminarPaciente(id)}
         >Eliminar</button>
       </div>
     </div>  
