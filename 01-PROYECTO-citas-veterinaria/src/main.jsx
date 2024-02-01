8454de438d80c9480220ea8890928447a0ef7ed0
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2024-01-31 21:25:45 -0300

    detectando y seteando la informacion previa existente en LS para que esta no se resetee

--- 01-PROYECTO-citas-veterinaria/src/main.jsx
+++ 01-PROYECTO-citas-veterinaria/src/main.jsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client' // version web de react
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
