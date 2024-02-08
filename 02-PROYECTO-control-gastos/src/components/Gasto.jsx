a0af420c8b80563788c3be0340493defe7860e9f
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2024-02-08 17:43:20 -0300

    formateando las fechas en el componente

--- 02-PROYECTO-control-gastos/src/components/Gasto.jsx
+++ 02-PROYECTO-control-gastos/src/components/Gasto.jsx
@@ -1,6 +1,8 @@
+import { formatearFecha } from "../helpers";
+
 const Gasto = ({ gasto }) => {
 
-  const { categoria, nombre, cantidad, id } = gasto;
+  const { categoria, nombre, cantidad, id, fecha } = gasto;
   return (
     <div className="gasto sombra">
       <div className="contenido-gasto">
@@ -8,9 +10,14 @@ const Gasto = ({ gasto }) => {
         <div className="descripcion-gasto">
           <p className="categoria">{categoria}</p>
           <p className="nombre-gasto">{nombre}</p>
-
+          <p className="fecha-gasto">
+            Agregado el: {''}
+            <span>{formatearFecha(fecha)}</span>
+          </p>
         </div>
       </div>
+
+      <div className="cantidad-gasto">${cantidad}</div>
     </div>
   )
 }
