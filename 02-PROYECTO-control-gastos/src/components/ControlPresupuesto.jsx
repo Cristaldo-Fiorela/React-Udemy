8e8647a224afe62cf80d91d173e17e9ad347adaf
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2024-02-14 19:52:00 -0300

    calculando presupuesto disponible

--- 02-PROYECTO-control-gastos/src/components/ControlPresupuesto.jsx
+++ 02-PROYECTO-control-gastos/src/components/ControlPresupuesto.jsx
@@ -7,7 +7,9 @@ const ControlPresupuesto = ({ gastos, presupuesto }) => {
 
   useEffect(() => {
     const totalGastado = gastos.reduce( (total, gasto) => gasto.cantidad + total, 0);
+    const totalDisponible = presupuesto - totalGastado;
 
+    setDisponible(totalDisponible);
     setGastado(totalGastado);
   }, [gastos])
 
