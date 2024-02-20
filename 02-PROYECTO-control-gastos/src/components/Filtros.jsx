770923ce56cb9721413ca6a8507b1db30d472c73
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2024-02-19 21:09:49 -0300

    mostrando los gastos filtrados

--- 02-PROYECTO-control-gastos/src/components/Filtros.jsx
+++ 02-PROYECTO-control-gastos/src/components/Filtros.jsx
@@ -11,7 +11,7 @@ const Filtros = ({ filtro, setFiltro }) => {
             id="categorias"
             onChange={(e) => setFiltro(e.target.value)}
           >
-            <option value="">---SELECCIONE---</option>
+            <option value="">--- Todas las categorias ---</option>
             <option value="ahorro">Ahorro</option>
             <option value="comida">Comida</option>
             <option value="casa">Casa</option>
